4e7983a969c260b005db1b5bee421ec65b2c231b
chilamvo015 <87653643+chilamvo015@users.noreply.github.com>
2021-07-19 20:42:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/vo.c.lam.7' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
